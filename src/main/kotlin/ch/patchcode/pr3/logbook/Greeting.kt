9424cab8787954d55214984edbd2d7861de5ac2b
Christian Fuchs <cfuchs@patchcode.ch>
2018-05-01 21:27:08 +0200

    + copied Greeting example from https://kotlinlang.org/docs/tutorials/spring-boot-restful.html

---
+++ src/main/kotlin/ch/patchcode/pr3/logbook/Greeting.kt
@@ -0,0 +1,6 @@
+package ch.patchcode.pr3.logbook
+
+data class Greeting(
+		var id: Long? = null,
+		var content: String
+)
---
+++ src/main/kotlin/ch/patchcode/pr3/logbook/PortRoyale3LogbookApplication.kt
@@ -0,0 +1,11 @@
+package ch.patchcode.pr3.logbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortRoyale3LogbookApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortRoyale3LogbookApplication>(*args)
+}
