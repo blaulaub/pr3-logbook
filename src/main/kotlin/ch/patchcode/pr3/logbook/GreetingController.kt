3a7abc2b2fa971be392116dba3580b49740856a5
Christian Fuchs <cfuchs@patchcode.ch>
2018-05-01 21:53:20 +0200

    + update Kotlin version and configure datasource

--- src/main/kotlin/ch/patchcode/pr3/logbook/GreetingController.kt
+++ src/main/kotlin/ch/patchcode/pr3/logbook/GreetingController.kt
@@ -13,6 +13,7 @@ class GreetingController {
 	@GetMapping("/greeting")
 	fun greeting(
 			@RequestParam(value = "name", defaultValue = "World") name: String
-	) = Greeting(counter.incrementAndGet(), name)
+	) =
+			Greeting(counter.incrementAndGet(), name)
 
 }
--- src/main/kotlin/ch/patchcode/pr3/logbook/PortRoyale3LogbookApplication.kt
+++ src/main/kotlin/ch/patchcode/pr3/logbook/PortRoyale3LogbookApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PortRoyale3LogbookApplication
+class PortRoyale3LogbookApplication {}
 
 fun main(args: Array<String>) {
     runApplication<PortRoyale3LogbookApplication>(*args)
