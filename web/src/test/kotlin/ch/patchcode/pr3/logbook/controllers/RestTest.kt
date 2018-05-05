a39859a93415c4c4cde9c46c05ee6a42e573d3a6
Christian Fuchs <cfuchs@patchcode.ch>
2018-05-05 12:07:18 +0200

    + added jacksonxml object mapper dependencies and hamcrest dependencies

--- web/src/test/kotlin/ch/patchcode/pr3/logbook/controllers/RestTest.kt
+++ web/src/test/kotlin/ch/patchcode/pr3/logbook/controllers/RestTest.kt
@@ -1,19 +1,24 @@
 package ch.patchcode.pr3.logbook.controllers
 
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-
+import ch.patchcode.pr3.logbook.objects.Game
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import ch.patchcode.pr3.logbook.PortRoyale3LogbookApplication
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -30,10 +35,13 @@ class RestTest {
 	}
 
 	@Test
+	@Transactional
 	fun test() {
 		val result = mvc.perform(post("/games").param("captainsName", "Morgan"))
-		result
-				.andDo(print())
-				.andExpect(status().isCreated())
+
+		result.andExpect(status().isCreated())
+		val content = result.andReturn().getResponse().getContentAsString()
+		val game = ObjectMapper().registerKotlinModule().registerModule(JavaTimeModule()).readValue(content, Game::class.java)
+		assertThat(game.captainsName, equalTo("Morgan"))
 	}
 }
