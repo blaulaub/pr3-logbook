c1ed4519238c0eff741de52e429d1b511dfb14ba
Christian Fuchs <cfuchs@patchcode.ch>
2018-05-06 17:24:51 +0200

    + changed angular game to games

--- angular-gui/src/app/entities/game.ts
+++ angular-gui/src/app/entities/game.ts
@@ -1,4 +1,5 @@
 export class Game {
   id: number;
   captainsName: string;
+  created: Date;
 }
--- angular-gui/src/app/games/games.component.ts
+++ angular-gui/src/app/games/games.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../entities.game.ts';
+import { Game } from '../entities/game';
 
 @Component({
   selector: 'app-games',
@@ -8,10 +8,18 @@ import { Game } from '../entities.game.ts';
 })
 export class GamesComponent implements OnInit {
 
-  game: Game = {
-    id: 1,
-    captainsName: 'Morgan'
-  };
+  games: Game[] = [
+    {
+      id: 10,
+      captainsName: 'Morgan',
+      created: new Date("2018-05-06T17:09:14.682")
+    },
+    {
+      id: 11,
+      captainsName: 'Störtebecker',
+      created: new Date("2018-05-06T17:19:00.000")
+    }
+  ];
 
   constructor() { }
 
