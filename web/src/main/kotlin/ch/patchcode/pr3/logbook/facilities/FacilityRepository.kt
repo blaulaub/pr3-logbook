3466da3f278442805d9dee724d81f773d36045b0
Christian Fuchs <cfuchs@patchcode.ch>
2018-06-02 23:59:10 +0200

    + use plain CrudRepo

--- web/src/main/kotlin/ch/patchcode/pr3/logbook/facilities/FacilityRepository.kt
+++ web/src/main/kotlin/ch/patchcode/pr3/logbook/facilities/FacilityRepository.kt
@@ -1,9 +1,9 @@
 package ch.patchcode.pr3.logbook.facilities
 
 import ch.patchcode.pr3.logbook.games.GameJpa
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 
-interface FacilityRepository : JpaRepository<FacilityJpa, Long> {
+interface FacilityRepository : CrudRepository<FacilityJpa, Long> {
 	fun findByGame(game: GameJpa): Iterable<FacilityJpa>
 	fun deleteByGameAndId(game: GameJpa, id: Long)
 }
